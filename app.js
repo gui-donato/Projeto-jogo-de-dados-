59f057405ef888d46d0f26b3e94e29787ffd4294
Guilherme Donato <guidonato.dev@gmail.com>
2026-02-12 11:04:15 -0300

    Atualização do jogo de dados - imagens iniciais e botão dinâmico

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
+const botao = document.getElementById("botaoJogar");
+const titulo = document.querySelector("h1");
+const img1 = document.getElementById("img1");
+const img2 = document.getElementById("img2");
 
-let randomNumero1 = Math.floor(Math.random() * 6) + 1
+botao.addEventListener("click", jogar);
 
-let randomDiceImg = 'dice' + randomNumero1 + '.png'
-let randomImgSource = 'images/' + randomDiceImg
+function jogar() {
 
-let img1 = document.querySelectorAll('img')[0]
+  let randomNumero1 = Math.floor(Math.random() * 6) + 1;
+  let randomNumero2 = Math.floor(Math.random() * 6) + 1;
 
-img1.setAttribute('src', randomImgSource)
+  // Atualiza imagens
+  img1.setAttribute("src", "images/dice" + randomNumero1 + ".png");
+  img2.setAttribute("src", "images/dice" + randomNumero2 + ".png");
 
+  // Resultado
+  if (randomNumero1 > randomNumero2) {
+    titulo.innerHTML = "🏆 Jogador 1 ganhou";
+  } 
+  else if (randomNumero2 > randomNumero1) {
+    titulo.innerHTML = "🏆 Jogador 2 ganhou";
+  } 
+  else {
+    titulo.innerHTML = "🤝 O jogo empatou";
+  }
 
-
-
-let randomNumero2 = Math.floor(Math.random() * 6) + 1
-
-let raqndomImgSource2 = 'images/dice' + randomNumero2 + '.png'
-
-document.querySelectorAll('img')[1].setAttribute('src', raqndomImgSource2)
-
-
-if (randomNumero1 > randomNumero2) { 
-    document.querySelector('h1').innerHTML = '🏆 Jogador 1 ganhou'
-
-}
-
-else if (randomNumero2 > randomNumero1) { 
-    document.querySelector('h1').innerHTML = '🏆 Jogador 2 ganhou'
-
-}
-else   { 
-    document.querySelector('h1').innerHTML = '🤝 O jogo empatou'
-
+  // Muda texto do botão
+  botao.innerHTML = "🎲 Jogar novamente";
 }
